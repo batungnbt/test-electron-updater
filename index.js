5f0e2fc90abdda7f118f7fc562a0ba5e16ad6524
Nguyen Ba Tung <batungnbt@gmail.com>
2024-02-23 18:15:32 +0700

    tung

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
+const { autoUpdater } = require("electron-updater")
+const log = require('electron-log');
 
-function createWindow () {
+log.initialize();
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -16,12 +20,27 @@ function createWindow () {
 app.whenReady().then(() => {
   createWindow()
 
+  autoUpdater.checkForUpdatesAndNotify();
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
   })
 })
+
+autoUpdater.on('update-available', () => {
+  log.info('Update available.');
+});
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  log.info('Update downloaded; will install now');
+});
+
+autoUpdater.on('error', (error) => {
+  log.error('There was a problem updating the application');
+  log.error(error);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
